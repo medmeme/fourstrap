17ee894eb07d885ede2a25c878959b6d87d48788
James Bernard <j.bernard@medmeme.com>
2017-10-18 16:35:18 -0400

    Keep it simple, import jQuery and popper from outside like before

--- build/stamp.js
+++ build/stamp.js
@@ -15,21 +15,16 @@ fs.readFile('package.json', (err, data) => {
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  */
 
-  /*
-    Modded By JB @ Fourstrap 10/18/17
-    - We are comenting this out because we are including jquery
+if (typeof jQuery === 'undefined') {
+  throw new Error('Bootstrap\\'s JavaScript requires jQuery. jQuery must be included before Bootstrap\\'s JavaScript.')
+}
 
-    if (typeof jQuery === 'undefined') {
-      throw new Error('Bootstrap\\'s JavaScript requires jQuery. jQuery must be included before Bootstrap\\'s JavaScript.')
-    }
-
-    (function ($) {
-      var version = $.fn.jquery.split(' ')[0].split('.')
-      if ((version[0] < 2 && version[1] < 9) || (version[0] == 1 && version[1] == 9 && version[2] < 1) || (version[0] >= 4)) {
-        throw new Error('Bootstrap\\'s JavaScript requires at least jQuery v1.9.1 but less than v4.0.0')
-      }
-    })(jQuery);
-  */
+(function ($) {
+  var version = $.fn.jquery.split(' ')[0].split('.')
+  if ((version[0] < 2 && version[1] < 9) || (version[0] == 1 && version[1] == 9 && version[2] < 1) || (version[0] >= 4)) {
+    throw new Error('Bootstrap\\'s JavaScript requires at least jQuery v1.9.1 but less than v4.0.0')
+  }
+})(jQuery);
 
 (function () {
 `
--- js/src/fourstrap/dependencies.js
+++
@@ -1,8 +0,0 @@
-import jQuery from 'jquery'
-import popper from 'popper.js'
-
-// Bootstrap 4 depends on jQuery so we want to build it with it.
-window.jQuery = jQuery
-
-// Bootstrap 4 also depends on popper.js again bundle it with it.
-window.Popper = popper
